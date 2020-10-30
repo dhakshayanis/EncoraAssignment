c274916b7fba0d0564a0d4e3623a88f8b9b447ec
Thiyagu <qateam@inniveinc.com>
2020-10-30 16:57:02 +0530

    Norwood student Learning page deleted

--- cypress/integration/Applications/DemoApplication/Common/NavigateToPage.js
+++ cypress/integration/Applications/DemoApplication/Common/NavigateToPage.js
@@ -5,11 +5,11 @@ class NavigateToPage{
         Cypress.on('uncaught:exception', (err, runnable) => {
             // returning false here prevents Cypress from failing the test
             return false
-          })/*
-          cy.get('[name="Email"]').type('dhakshayanis@inniveinc.com')//('demouser@inniveinc.com')
+          })
+          cy.get('[name="Email"]').type('tambalavanan@inniveinc.com')//('demouser@inniveinc.com')
           cy.get('#next').click()
-          cy.get('[type="password"]').type('Eashwari')//('Innive@23')
-          cy.get('#submit').click()*/
+          cy.get('[type="password"]').type('9486304276')//('Innive@23')
+          cy.get('#submit').click()
           cy.wait(10000)
           cy.wait(20000)
           cy.get('[class=external-generic-component-child2]').contains('Remote').click()
--- cypress/integration/Applications/Norwood/Common/PageHeader.js
+++ cypress/integration/Applications/Norwood/Common/PageHeader.js
@@ -4,14 +4,14 @@ class PageHeader{
         cy.get('[class=logout]').should('have.text','Logout')
 
         //UsernameCheck
-        cy.get('[class=user_name]').should('have.text','dhakshayanis')
+        cy.get('[class=user_name]').should('have.text','tambalavanan')
 
         //logo check
-        cy.get('[alt="WHOLE CHILD INTEGRATED DATA"]').should('have.attr','src','https://demo.remotelearning.innive.io/images/client_logo_boston.png')
+        cy.get('[alt="WHOLE CHILD INTEGRATED DATA"]').should('have.attr','src','https://dev-dot-nps-i360-k12-dev.uc.r.appspot.com/images/client_logo_norwood.png')
         return this
     }
     pageFooterCheck(){
-        cy.get('.brintell-framework-footer-text').should('have.text','Disclaimer: This information is the property of the Demo Public Schools and is intended solely for use by individuals expressly authorized by the District. This information is not to be reproduced in any form, viewed, distributed, or disclosed in any way to or by unauthorized individuals. The sharing of this information is subject to conditions set forth in applicable laws, regulations, and policies regarding privacy and confidentiality.')
+        cy.get('.brintell-framework-footer-text').should('have.text','Disclaimer: This information is the property of the Norwood Public Schools and is intended solely for use by individuals expressly authorized by the District. This information is not to be reproduced in any form, viewed, distributed, or disclosed in any way to or by unauthorized individuals. The sharing of this information is subject to conditions set forth in applicable laws, regulations, and policies regarding privacy and confidentiality.')
         return this
     }
 }
