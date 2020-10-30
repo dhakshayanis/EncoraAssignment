d435d8e59389a87b4dafcc047a066f9347b6f5a0
Thiyagu <qateam@inniveinc.com>
2020-10-30 16:55:58 +0530

    norwood

---
+++ cypress/integration/Applications/Norwood/Common/DateFilter.js
@@ -0,0 +1,12 @@
+class DateFilter{
+    startDateDefault(){
+        cy.get('[placeholder="Start Date"]').should('have.value','06-Apr-2020')
+        return this
+    }
+    endDateDefault(){
+        const todaysDate = Cypress.moment().format('DD-MMM-YYYY')
+        cy.get('[placeholder="End Date"]').should('have.value',todaysDate)
+        return this
+    }
+}
+export default DateFilter
