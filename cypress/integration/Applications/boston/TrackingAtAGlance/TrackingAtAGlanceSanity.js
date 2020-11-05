877e51a66c8c0631d3dda31e31308e475679a75b
Hemalathe <hemalatha@inniveinc.com>
2020-11-05 18:01:25 +0530

    eEdtech page final modification

--- cypress/integration/Applications/boston/TrackingAtAGlance/TrackingAtAGlanceSanity.js
+++ cypress/integration/Applications/boston/TrackingAtAGlance/TrackingAtAGlanceSanity.js
@@ -1,43 +1,58 @@
-import Dropdown from '../DropDown'
-import DropDownObjects from '../DropDownObjects'
+import Dropdown from '../Common/DropDown'
+import DropDownObjects from '../common/DropDownObjects'
+import TrackingAtAGlanceObjects from './TrackingAtAGlanceObjects'
 
 class TrackingAtAGlanceSanity{
     isTilesPresent(){
-        cy.get('[id="10"]').should('be.visible')//student not active
-        cy.get('[id="5"]').scrollIntoView()
-        cy.get('[id="5"]').should('be.visible')//student  active
-        cy.get('[id="4"]').scrollIntoView()
-        cy.get('[id="4"]').should('be.visible')//student devices
-        cy.get('[id="7"]').scrollIntoView()
-        cy.get('[id="7"]').should('be.visible')//application logins
-        cy.get('[id="8"]').scrollIntoView()
-        cy.get('[id="8"]').should('be.visible')//teacher active 
-        cy.get('[id="10"]').scrollIntoView() 
+        TrackingAtAGlanceSanity.isTileVisible(TrackingAtAGlanceObjects.dailyInactiveStudentsID)
+        TrackingAtAGlanceSanity.isTileVisible(TrackingAtAGlanceObjects.dailyActiveStudentsID)
+        TrackingAtAGlanceSanity.isTileVisible(TrackingAtAGlanceObjects.devicesDistributionID)
+        TrackingAtAGlanceSanity.isTileVisible(TrackingAtAGlanceObjects.applicationLoginsID)
+        TrackingAtAGlanceSanity.isTileVisible(TrackingAtAGlanceObjects.activeTeachersID)
+        TrackingAtAGlanceSanity.isTileVisible(TrackingAtAGlanceObjects.inactiveStudentsYtdID)
+        TrackingAtAGlanceSanity.isTileVisible(TrackingAtAGlanceObjects.weeklyActiveStudentsID)
         return this
+    }
+    static isTileVisible(tileID){
+        cy.get(tileID).scrollIntoView()
+        cy.get(tileID).should('be.visible')//student  active
+    }
+    static checkTileHeader(tileID){
+        cy.get(tileID).scrollIntoView()
+        //cy.get(tileID).within(()=>{
+            cy.contains(TrackingAtAGlanceObjects.getTileTitle(tileID))
+                    .should('have.text',TrackingAtAGlanceObjects.getTileTitle(tileID))//})
     }
     tilesHeaderCHeck(){
-        cy.contains('INACTIVESTUDENTS').should('have.text','INACTIVESTUDENTS')
-        cy.get('[id="5"]').scrollIntoView()
-        cy.get('[id="5"]').within(()=>{cy.contains('ACTIVESTUDENTS').should('have.text','ACTIVESTUDENTS')})
-        cy.get('[id="4"]').scrollIntoView()
-        cy.get('[id="4"]').within(()=>{cy.contains('DEVICESDISTRIBUTION').should('have.text','DEVICESDISTRIBUTION')})
-        cy.get('[id="7"]').scrollIntoView()
-        cy.get('[id="7"]').within(()=>{cy.contains('APPLICATION LOGINS').should('have.text','APPLICATION LOGINS')})
-        cy.get('[id="8"]').scrollIntoView()
-        cy.get('[id="8"]').within(()=>{cy.contains('ACTIVETEACHERS').should('have.text','ACTIVETEACHERS')})
-        cy.get('[id="10"]').scrollIntoView() 
+        TrackingAtAGlanceSanity.checkTileHeader(TrackingAtAGlanceObjects.dailyInactiveStudentsID)
+        TrackingAtAGlanceSanity.checkTileHeader(TrackingAtAGlanceObjects.dailyActiveStudentsID)
+        TrackingAtAGlanceSanity.checkTileHeader(TrackingAtAGlanceObjects.weeklyActiveStudentsID)
+        TrackingAtAGlanceSanity.checkTileHeader(TrackingAtAGlanceObjects.inactiveStudentsYtdID)
+        TrackingAtAGlanceSanity.checkTileHeader(TrackingAtAGlanceObjects.devicesDistributionID)
+        TrackingAtAGlanceSanity.checkTileHeader(TrackingAtAGlanceObjects.applicationLoginsID)
+        TrackingAtAGlanceSanity.checkTileHeader(TrackingAtAGlanceObjects.activeTeachersID)
+        cy.get(TrackingAtAGlanceObjects.dailyInactiveStudentsID).scrollIntoView() 
         return this
     }
+    static textCheck(tileID){
+        cy.contains(TrackingAtAGlanceObjects.getTitleText1(tileID))
+            .should('have.text',TrackingAtAGlanceObjects.getTitleText1(tileID))
+        if(TrackingAtAGlanceObjects.getTitleText2(tileID)==''){
+            cy.log('Text 2 is null for this tile')
+        }
+        else{
+            cy.get(tileID).contains(TrackingAtAGlanceObjects.getTitleText2(tileID))
+            .should('have.text',TrackingAtAGlanceObjects.getTitleText2(tileID))
+        }       
+    }    
     tilesTextCheck(){
-        cy.contains('Avg # of Inactive students').should('have.text','Avg # of Inactive students')
-        cy.get('[id="10"]').contains('Last 7 Days Avg').should('have.text','Last 7 Days Avg')
-        cy.contains('Avg # of Students Active').should('have.text','Avg # of Students Active')
-        cy.get('[id="5"]').contains('Last 7 Days Avg').should('have.text',' Last 7 Days Avg')
-        cy.contains('Devices Distributed').should('have.text','Devices Distributed')
-        cy.contains('Students With Devices').should('have.text','Students With Devices')
-        cy.contains('Average Number of Apps Accessed per Student').should('have.text','Average Number of Apps Accessed per Student')
-        cy.get('[id="8"]').contains('Avg # of Teachers Active').should('have.text','Avg # of Teachers Active')
-        cy.get('[id="8"]').contains('Last 7 Days').should('have.text','Last 7 Days Avg')
+        TrackingAtAGlanceSanity.textCheck(TrackingAtAGlanceObjects.dailyInactiveStudentsID)
+        TrackingAtAGlanceSanity.textCheck(TrackingAtAGlanceObjects.dailyActiveStudentsID)
+        TrackingAtAGlanceSanity.textCheck(TrackingAtAGlanceObjects.weeklyActiveStudentsID)
+        TrackingAtAGlanceSanity.textCheck(TrackingAtAGlanceObjects.inactiveStudentsYtdID)
+        TrackingAtAGlanceSanity.textCheck(TrackingAtAGlanceObjects.devicesDistributionID)
+        TrackingAtAGlanceSanity.textCheck(TrackingAtAGlanceObjects.applicationLoginsID)
+        TrackingAtAGlanceSanity.textCheck(TrackingAtAGlanceObjects.activeTeachersID)
         cy.get('[class=brintell-framework-page-title]').scrollIntoView()
         return this
     }
