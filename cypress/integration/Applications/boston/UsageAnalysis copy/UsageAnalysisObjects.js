968720ad80a5ec1a3109f4969a53c4e601a5931e
Hemalathe <hemalatha@inniveinc.com>
2020-11-06 21:41:23 +0530

    People and device final modification

---
+++ cypress/integration/Applications/boston/UsageAnalysis copy/UsageAnalysisObjects.js
@@ -0,0 +1,9 @@
+class UsageAnalysisObjects{
+    static pageTitle = "Usage Analysis"
+    static UsageAnalysisSchoolView = "School View"
+    static UsageAnalysisMapView = "Map View"
+    static UsageAnalysisDimensionView = "Dimension View"
+    static UsageAnalysisHeatMapAnalysisView = "Heat Map Analysis"
+    static UsageAnalysisTimeSeries = "Time Series"
+}
+export default UsageAnalysisObjects
