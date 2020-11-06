1e4446e2112d03e0cea6cd25c7ae06a105b5a8de
Hemalathe <hemalatha@inniveinc.com>
2020-11-06 11:50:19 +0530

    Tracking at a glance final modification

--- cypress/integration/Test/Sanity Test/Boston Application/SanityTrackingAtAGlanceTest.js
+++ cypress/integration/Test/Sanity Test/Boston Application/SanityTrackingAtAGlanceTest.js
@@ -1,31 +1,31 @@
-import NavigateToPage from '../../../Application/boston/Common/NavigateToPage'
-import PageTitleCheck from '../../../Application/boston/Common/PageTitleCheck'
-import PageHeader from '../../../Application/boston/Common/PageHeader'
-import TrackingAtAGlanceSanity from '../../../Application/boston/Common/TrackingAtAGlance/TrackingAtAGlanceSanity'
-import DateFilter from '../../../Application/boston/Common/DateFilter'
-import Dropdown from '../../../Application/boston/Common/DropDown'
-import TrackingAtAGlanceObjects from '../../../Application/boston/TrackingAtAGlance/TrackingAtAGlanceObjects'
-//import TrackingAtAGlanceObjects from './TrackingAtAGlance/TrackingAtAGlanceObjects'
-//import trackingAtAGlanceDropdown from './TrackingAtAGlance/DropDown'
+import NavigateToPage from '../../../Applications/boston/Common/NavigateToPage'
+import PageTitleCheck from '../../../Applications/boston/Common/PageTitleCheck'
+import PageHeader from '../../../Applications/boston/Common/PageHeader'
+import TrackingAtAGlanceSanity from '../../../Applications/boston/TrackingAtAGlance/TrackingAtAGlanceSanity'
+import DateFilter from '../../../Applications/boston/Common/DateFilter'
+import Dropdown from '../../../Applications/boston/Common/DropDown'
+import TrackingAtAGlanceObjects from '../../../Applications/boston/TrackingAtAGlance/TrackingAtAGlanceObjects'
+//import trackingAtAGlanceDropdown from './examples/DemoApplication/TrackingAtAGlance/DropDown'
 
 describe('one',function()
 {
-    beforeEach(function(){
-        cy.fixture('PageTitles').then(function(data){
-            this.data = data
-    })
-})
 it('navigate to Tracking At a Glance',function()
 {
     const np = new NavigateToPage
     np.goToTrackingAtAGlancePage()
     const titleCheck = new PageTitleCheck
-    titleCheck.PageTitleTest(this.data.TrackingAtAGlanceTitle)
+    titleCheck.PageTitleTest(TrackingAtAGlanceObjects.pagetitle)
 })
    it('verify username in page header ',function()
    {
     const header = new PageHeader
     header.pageHeaderCheck()
+   })
+   it('verify date filters ',function()
+   {
+        const date = new DateFilter
+        date.startDateDefault
+        date.endDateDefault
    })
    it('verify all tiles are present',function()
    {
@@ -41,12 +41,6 @@ it('navigate to Tracking At a Glance',function()
    {
     const text = new TrackingAtAGlanceSanity
     text.tilesTextCheck()
-   })
-   it('date filter',function()
-   {
-       const date = new DateFilter
-       date.startDateDefault()
-       date.endDateDefault()
    })
    it('dropdown sanity',function()
    {
