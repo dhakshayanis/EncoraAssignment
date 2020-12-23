40d40e3fff97fc2eab8c033868cb3a66d208c378
DhakshayaniSritharaboopathy <dhakshayanis@inniveinc.com>
2020-12-23 11:04:20 +0530

    Minor changes

--- cypress/integration/Applications/CentralFalls/Common/NavigateToPage.js
+++ cypress/integration/Applications/CentralFalls/Common/NavigateToPage.js
@@ -7,20 +7,20 @@ class NavigateToPage{
         Cypress.on('uncaught:exception', (err, runnable) => {
             // returning false here prevents Cypress from failing the test
             return false
-          })
+          })/*
           cy.get('[name="Email"]').type('dhakshayanis@inniveinc.com')//('demouser@inniveinc.com')
           cy.get('#next').click()
           cy.get('[type="password"]').type('Eashwari')//('Innive@23')
-          cy.get('#submit').click()
+          cy.get('#submit').click()*/
           cy.wait(10000)
           cy.wait(30000)
           cy.get('[class=external-generic-component-child2]').contains('Remote').click()
-          //cy.wait(30000)
+          cy.wait(30000)
           return this
         }
     goToPage(page)
     {
-        cy.wait(30000)
+        cy.wait(45000)
         cy.get('[class=long_label]').contains(page,{matchCase: false}).click({force : true})
         const titleCheck = new PageTitleCheck
         titleCheck.PageTitleTest(page)
